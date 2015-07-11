8b23ae4aeea83be993387a72fa10cb95853583e4
Frank Schibilla <fsh@frank-schibilla.de>
2015-07-11 19:36:15 +0200

    Learning Java
    Halle nur zum Üben

---
+++ Klabautermann.java
@@ -0,0 +1,13 @@
+package onlineProject;
+
+public class Klabautermann {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Klabautermann");
+		System.out.println("Klabauterfrau");
+		System.out.println("Klabauterkind");
+		System.out.println("im Schiffe sind.");
+	}
+
+}
---
+++ Neptun.java
@@ -0,0 +1,9 @@
+package onlineProject;
+
+public class Neptun {
+
+	public static void main ( String[] args )
+	  {
+	    System.out.println("Hallo Neptun!");
+	  }
+	}
